fcd400679a503f20cd2bff8996b929bbd7209227
wykhuh <wykhuh@users.noreply.github.com>
2019-02-14 16:34:30 -0800

    add metadata for portfolio plugin

---
+++ plugins/my_portfolio.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: My Portfolio
+ * Plugin URI:
+ * Description: Display portfolio
+ * Version:     0.1
+ * Author:      wyk
+ * Author URI:  https://author.example.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: my-portfolio
+ * Domain Path:
+ */
