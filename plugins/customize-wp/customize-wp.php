139c08ba07887ddecca00f12438e20cecb5b3df3
wykhuh <wykhuh@users.noreply.github.com>
2019-02-20 10:23:14 -0800

    add plugin to top level admin menu

--- plugins/customize-wp/customize-wp.php
+++ plugins/customize-wp/customize-wp.php
@@ -16,3 +16,62 @@ if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
+// display the plugin settings page
+function customize_wp_display_settings_page() {
+
+	// check if user is allowed access
+	if ( ! current_user_can( 'manage_options' ) ) return;
+
+	?>
+
+	<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+		<form action="options.php" method="post">
+
+			<?php
+
+			// output security fields
+			settings_fields( 'customize_wp_options' );
+
+			// output setting sections
+			do_settings_sections( 'customize_wp' );
+
+			// submit button
+			submit_button();
+
+			?>
+
+		</form>
+	</div>
+
+	<?php
+
+}
+
+// add top-level administrative menu
+function customize_wp_add_toplevel_menu() {
+
+	/*
+		add_menu_page(
+			string   $page_title,
+			string   $menu_title,
+			string   $capability,
+			string   $menu_slug,
+			callable $function = '',
+			string   $icon_url = '',
+			int      $position = null
+		)
+	*/
+
+	add_menu_page(
+		'Customize WP Settings',
+		'Customize WP',
+		'manage_options',
+		'customize_wp',
+		'customize_wp_display_settings_page',
+		'dashicons-admin-generic',
+		null
+	);
+
+}
+add_action( 'admin_menu', 'customize_wp_add_toplevel_menu' );
--- z_notes/plugin_development/3_building_plugin.md
+++ z_notes/plugin_development/3_building_plugin.md
@@ -2,3 +2,15 @@ https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
 
 https://developer.wordpress.org/plugins/wordpress-org/how-your-readme-txt-works/
 
+==
+
+add plugin to admin menu
+
+top-level menu
+- may contain submenus
+- best for plugins with multiple settings pages
+
+
+sub-level menu
+- add to existing top-level menu
+- best for plugins with one settings page
