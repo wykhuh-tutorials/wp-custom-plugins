61edc9215a50cb82998a8bd2a4e6fa43d19c1188
wykhuh <wykhuh@users.noreply.github.com>
2019-02-25 14:16:00 -0800

    display form fields using callbacks

--- plugins/customize-wp/admin/settings-callbacks.php
+++ plugins/customize-wp/admin/settings-callbacks.php
@@ -23,30 +23,102 @@ function customize_wp_callback_section_admin() {
 
 // callback: text field
 function customize_wp_callback_field_text( $args ) {
-	// todo: add callback functionality..
-	echo 'This will be a text field.';
+	$options = get_option( 'customize_wp_options', customize_wp_options_default() );
+
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$value = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+	echo '<input id="customize_wp_options_'. $id .'" name="customize_wp_options['. $id .']" type="text" size="40" value="'. $value .'"><br />';
+	echo '<label for="customize_wp_options_'. $id .'">'. $label .'</label>';
 }
+
 
 // callback: radio field
 function customize_wp_callback_field_radio( $args ) {
-	// todo: add callback functionality..
-	echo 'This will be a radio field.';
+	$options = get_option( 'customize_wp_options', customize_wp_options_default() );
+
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$selected_option = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+	$radio_options = array(
+
+		'enable'  => 'Enable custom styles',
+		'disable' => 'Disable custom styles'
+
+	);
+
+	foreach ( $radio_options as $value => $label ) {
+
+		$checked = checked( $selected_option === $value, true, false );
+
+		echo '<label><input name="customize_wp_options['. $id .']" type="radio" value="'. $value .'"'. $checked .'> ';
+		echo '<span>'. $label .'</span></label><br />';
+	}
 }
+
 
 // callback: textarea field
 function customize_wp_callback_field_textarea( $args ) {
-	// todo: add callback functionality..
-	echo 'This will be a textarea.';
+	$options = get_option( 'customize_wp_options', customize_wp_options_default() );
+
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$allowed_tags = wp_kses_allowed_html( 'post' );
+
+	$value = isset( $options[$id] ) ? wp_kses( stripslashes_deep( $options[$id] ), $allowed_tags ) : '';
+
+	echo '<textarea id="customize_wp_options_'. $id .'" name="customize_wp_options['. $id .']" rows="5" cols="50">'. $value .'</textarea><br />';
+	echo '<label for="customize_wp_options_'. $id .'">'. $label .'</label>';
 }
 
 // callback: checkbox field
 function customize_wp_callback_field_checkbox( $args ) {
-	// todo: add callback functionality..
-	echo 'This will be a checkbox.';
+	$options = get_option( 'customize_wp_options', customize_wp_options_default() );
+
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$checked = isset( $options[$id] ) ? checked( $options[$id], 1, false ) : '';
+
+	echo '<input id="customize_wp_options_'. $id .'" name="customize_wp_options['. $id .']" type="checkbox" value="1"'. $checked .'> ';
+	echo '<label for="customize_wp_options_'. $id .'">'. $label .'</label>';
+
 }
 
 // callback: select field
 function customize_wp_callback_field_select( $args ) {
-	// todo: add callback functionality..
-	echo 'This will be a select menu.';
+	$options = get_option( 'customize_wp_options', customize_wp_options_default() );
+
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$selected_option = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+	$select_options = array(
+
+		'default'   => 'Default',
+		'light'     => 'Light',
+		'blue'      => 'Blue',
+		'coffee'    => 'Coffee',
+		'ectoplasm' => 'Ectoplasm',
+		'midnight'  => 'Midnight',
+		'ocean'     => 'Ocean',
+		'sunrise'   => 'Sunrise',
+
+	);
+
+	echo '<select id="customize_wp_options_'. $id .'" name="customize_wp_options['. $id .']">';
+
+	foreach ( $select_options as $value => $option ) {
+		$selected = selected( $selected_option === $value, true, false );
+		echo '<option value="'. $value .'"'. $selected .'>'. $option .'</option>';
+	}
+
+	echo '</select> <label for="customize_wp_options_'. $id .'">'. $label .'</label>';
 }
+
--- plugins/customize-wp/customize-wp.php
+++ plugins/customize-wp/customize-wp.php
@@ -26,3 +26,17 @@ if ( is_admin())  {
   require_once plugin_dir_path(__FILE__) . 'admin/settings-register.php';
 }
 
+// default plugin options
+function customize_wp_options_default() {
+
+	return array(
+		'custom_url'     => 'https://wordpress.org/',
+		'custom_title'   => 'Powered by WordPress',
+		'custom_style'   => 'disable',
+		'custom_message' => '<p class="custom-message">My custom message</p>',
+		'custom_footer'  => 'Special message for users',
+		'custom_toolbar' => false,
+		'custom_scheme'  => 'default',
+	);
+
+}
