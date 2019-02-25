c557eecee2922e70caaed910f4f7d834672d3128
wykhuh <wykhuh@users.noreply.github.com>
2019-02-25 13:33:03 -0800

    add hardcode placeholder for fields

--- plugins/customize-wp/customize-wp.php
+++ plugins/customize-wp/customize-wp.php
@@ -69,6 +69,83 @@ function custom_wp_register_settings() {
 		'customize_wp'
 	);
 
+	/*
+
+	add_settings_field(
+    	string   $id: used to retrieve setting from database ,
+		string   $title: label,
+		callable $callback: display markup,
+		string   $page: page to display setting,
+		string   $section = 'default':  section to display setting,
+		         $section is the same as "add_settings_section -> section"
+		array    $args = []: data to pass to callback
+	);
+
+	*/
+
+	add_settings_field(
+		'custom_url',
+		'Custom URL',
+		'customize_wp_callback_field_text',
+		'customize_wp',
+		'customize_wp_section_login',
+		[ 'id' => 'custom_url', 'label' => 'Custom URL for the login logo link' ]
+	);
+
+	add_settings_field(
+		'custom_title',
+		'Custom Title',
+		'customize_wp_callback_field_text',
+		'customize_wp',
+		'customize_wp_section_login',
+		[ 'id' => 'custom_title', 'label' => 'Custom title attribute for the logo link' ]
+	);
+
+	add_settings_field(
+		'custom_style',
+		'Custom Style',
+		'customize_wp_callback_field_radio',
+		'customize_wp',
+		'customize_wp_section_login',
+		[ 'id' => 'custom_style', 'label' => 'Custom CSS for the Login screen' ]
+	);
+
+	add_settings_field(
+		'custom_message',
+		'Custom Message',
+		'customize_wp_callback_field_textarea',
+		'customize_wp',
+		'customize_wp_section_login',
+		[ 'id' => 'custom_message', 'label' => 'Custom text and/or markup' ]
+	);
+
+	add_settings_field(
+		'custom_footer',
+		'Custom Footer',
+		'customize_wp_callback_field_text',
+		'customize_wp',
+		'customize_wp_section_admin',
+		[ 'id' => 'custom_footer', 'label' => 'Custom footer text' ]
+	);
+
+	add_settings_field(
+		'custom_toolbar',
+		'Custom Toolbar',
+		'customize_wp_callback_field_checkbox',
+		'customize_wp',
+		'customize_wp_section_admin',
+		[ 'id' => 'custom_toolbar', 'label' => 'Remove new post and comment links from the Toolbar' ]
+	);
+
+	add_settings_field(
+		'custom_scheme',
+		'Custom Scheme',
+		'customize_wp_callback_field_select',
+		'customize_wp',
+		'customize_wp_section_admin',
+		[ 'id' => 'custom_scheme', 'label' => 'Default color scheme for new users' ]
+	);
+
 }
 add_action( 'admin_init', 'custom_wp_register_settings' );
 
@@ -87,5 +164,35 @@ function customize_wp_callback_section_login() {
 // callback: admin section
 function customize_wp_callback_section_admin() {
 	echo '<p>These settings enable you to customize the WP Admin Area.</p>';
+}
+
+// callback: text field
+function customize_wp_callback_field_text( $args ) {
+	// todo: add callback functionality..
+	echo 'This will be a text field.';
+}
+
+// callback: radio field
+function customize_wp_callback_field_radio( $args ) {
+	// todo: add callback functionality..
+	echo 'This will be a radio field.';
+}
+
+// callback: textarea field
+function customize_wp_callback_field_textarea( $args ) {
+	// todo: add callback functionality..
+	echo 'This will be a textarea.';
+}
+
+// callback: checkbox field
+function customize_wp_callback_field_checkbox( $args ) {
+	// todo: add callback functionality..
+	echo 'This will be a checkbox.';
+}
+
+// callback: select field
+function customize_wp_callback_field_select( $args ) {
+	// todo: add callback functionality..
+	echo 'This will be a select menu.';
 }
 
